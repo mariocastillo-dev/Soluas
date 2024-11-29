6c49f5ab385dd8f50c58d7395d9b78e45c3a468f
MacInvestment <mario_castillo_dev>
2024-11-28 20:45:39 -0500

    Actualización: implementaciones CRUD, mejoras en main.py y corrección de errores

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models.base import Base
 
-DATABASE_URL = "mysql+mysqlconnector://Admin:Admin@localhost:3306/soluas"
+DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/soluas_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
