2d41689b573a09624b4995ed0924cb5d2bd90c54
zarkaresgame <jadenv2000@gmail.com>
2024-12-10 13:40:56 -0500

    Implementacion de Kpis

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from app.route.user_routes import router as user_router
 from app.route.role_routes import router as role_router
 from app.route.data_routes import router as data_router
-from app.route.auth_routes import router as auth_router
+from app.route.kpi_routes import router as kpi_router
 
 app = FastAPI()
 
-app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+# Incluir las rutas
 app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(role_router, prefix="/roles", tags=["Roles"])
 app.include_router(data_router, prefix="/data", tags=["Data"])
+app.include_router(kpi_router, prefix="/kpis", tags=["KPIs"])
 
 @app.get("/")
 def read_root():
